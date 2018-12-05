2b2741e41be0ea677c6b17aef7f0374aac2b7213
Ivan <kostko_i@yahoo.com>
2018-12-06 00:48:36 +0100

    DevOps for integration tests

---
+++ test_integration.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# This scripts runs all the  integration tests. It uses Docker Compose to
+# set up an environment. It is not supposed to run in a Docker container.
+#
+
+echo "Stopping and removing running containers"
+docker-compose stop
+docker-compose rm -f
+echo "Running integration tests..."
+docker-compose run --rm -T test_integration go test -v -tags integration ./...
